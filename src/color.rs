858f6b6994192f76a229077c35c6ecb2b4d2da84
tom-anders <13141438+tom-anders@users.noreply.github.com>
2024-04-18 20:54:25 +0200

    refactor: clamp value inside Color struct (chapter 8.2)

--- src/color.rs
+++ src/color.rs
@@ -4,22 +4,19 @@ pub struct Color(Vec3);
 
 impl Color {
     pub fn from_rgb_float(v: Vec3) -> Self {
-        debug_assert!(
-            (0.0..=1.0).contains(&v.x) && (0.0..=1.0).contains(&v.y) && (0.0..=1.0).contains(&v.z)
-        );
         Self(v)
     }
 
     pub fn red(&self) -> u8 {
-        (self.0.x * 255.999) as u8
+        (self.0.x.clamp(0.0, 0.999) * 256.0) as u8
     }
 
     pub fn green(&self) -> u8 {
-        (self.0.y * 255.999) as u8
+        (self.0.y.clamp(0.0, 0.999) * 256.0) as u8
     }
 
     pub fn blue(&self) -> u8 {
-        (self.0.z * 255.999) as u8
+        (self.0.z.clamp(0.0, 0.999) * 256.0) as u8
     }
 }
 
