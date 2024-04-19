3a72d0d396b066b0895da3e5f40fd5d280f95a1d
tom-anders <13141438+tom-anders@users.noreply.github.com>
2024-04-19 16:59:29 +0200

    feat: gamma correction (chapter 9.5)

--- src/color.rs
+++ src/color.rs
@@ -2,21 +2,25 @@ use crate::math::vec3::Vec3;
 
 pub struct Color(Vec3);
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    linear_component.max(0.0).sqrt()
+}
+
 impl Color {
     pub fn from_rgb_float(v: Vec3) -> Self {
         Self(v)
     }
 
-    pub fn red(&self) -> u8 {
-        (self.0.x.clamp(0.0, 0.999) * 256.0) as u8
+    fn red(&self) -> u8 {
+        (linear_to_gamma(self.0.x).clamp(0.0, 0.999) * 256.0) as u8
     }
 
-    pub fn green(&self) -> u8 {
-        (self.0.y.clamp(0.0, 0.999) * 256.0) as u8
+    fn green(&self) -> u8 {
+        (linear_to_gamma(self.0.y).clamp(0.0, 0.999) * 256.0) as u8
     }
 
-    pub fn blue(&self) -> u8 {
-        (self.0.z.clamp(0.0, 0.999) * 256.0) as u8
+    fn blue(&self) -> u8 {
+        (linear_to_gamma(self.0.z).clamp(0.0, 0.999) * 256.0) as u8
     }
 }
 
