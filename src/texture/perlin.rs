4abf310735b1585c4a8665fd91df2a240b680026
tom-anders <13141438+tom-anders@users.noreply.github.com>
2024-04-24 21:04:44 +0200

    feat: smoothen perlin noise

--- src/texture/perlin.rs
+++ src/texture/perlin.rs
@@ -1,5 +1,4 @@
-use std::thread;
-
+use itertools::iproduct;
 use rand::{seq::SliceRandom, thread_rng, Rng};
 
 use crate::math::Point3;
@@ -20,6 +19,18 @@ fn perlin_generate_perm() -> [usize; NUM_POINTS] {
     p
 }
 
+fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    iproduct!(0..2, 0..2, 0..2)
+        .map(|(i, j, k)| {
+            let (i, j, k) = (i as f64, j as f64, k as f64);
+            (i * u + (1.0 - i) * (1.0 - u))
+                * (j * v + (1.0 - j) * (1.0 - v))
+                * (k * w + (1.0 - k) * (1.0 - w))
+                * c[i as usize][j as usize][k as usize]
+        })
+        .sum()
+}
+
 impl Default for Perlin {
     fn default() -> Self {
         Self::new()
@@ -37,10 +48,27 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
+        let hermitian = |x| x * x * (3.0 - 2.0 * x);
 
-        self.randfloat[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]]
+        let u = hermitian(p.x() - p.x().floor());
+        let v = hermitian(p.y() - p.y().floor());
+        let w = hermitian(p.z() - p.z().floor());
+
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+
+        use std::array::from_fn;
+        let c: [[[f64; 2]; 2]; 2] = from_fn(|di| {
+            from_fn(|dj| {
+                from_fn(|dk| {
+                    self.randfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]]
+                })
+            })
+        });
+
+        trilinear_interp(&c, u, v, w)
     }
 }
